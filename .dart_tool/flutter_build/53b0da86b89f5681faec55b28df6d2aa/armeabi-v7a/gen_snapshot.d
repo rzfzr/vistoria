d329192d2c1c85e25ec0383755b4c2d1ff89c4ae
Rafael Goncalves <capitao.rafa@gmail.com>
2020-03-14 16:53:30 -0300

    Initial commit
    Code from Kaio

---
+++ .dart_tool/flutter_build/53b0da86b89f5681faec55b28df6d2aa/armeabi-v7a/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: F:\scr\flutter\bin\cache\artifacts\engine\android-arm-release\windows-x64\gen_snapshot
---
+++ lib/model/Cliente.dart
@@ -0,0 +1,133 @@
+class Cliente {
+  String _empresa;
+  String _endCompleto;
+  String _cidade;
+  String _UF;
+  String _bairro;
+  String _numero;
+  String _telefone;
+  String _email;
+
+  String _executor;
+  String _cnpj;
+  String _endereço;
+  // endereço, cidade, bairro
+  String _responsavelTec;
+  String _CREA;
+  String _aparelho;
+  String _potencia;
+  String _localdoAparelho;
+  String _numeroDoPatrimonio;
+
+  String _quantidadeArCond;
+
+  String get empresa => _empresa;
+
+  set empresa(String value) {
+    _empresa = value;
+  }
+
+  String get endCompleto => _endCompleto;
+
+  String get quantidadeArCond => _quantidadeArCond;
+
+  set quantidadeArCond(String value) {
+    _quantidadeArCond = value;
+  }
+
+  String get numeroDoPatrimonio => _numeroDoPatrimonio;
+
+  set numeroDoPatrimonio(String value) {
+    _numeroDoPatrimonio = value;
+  }
+
+  String get localdoAparelho => _localdoAparelho;
+
+  set localdoAparelho(String value) {
+    _localdoAparelho = value;
+  }
+
+  String get potencia => _potencia;
+
+  set potencia(String value) {
+    _potencia = value;
+  }
+
+  String get aparelho => _aparelho;
+
+  set aparelho(String value) {
+    _aparelho = value;
+  }
+
+  String get CREA => _CREA;
+
+  set CREA(String value) {
+    _CREA = value;
+  }
+
+  String get responsavelTec => _responsavelTec;
+
+  set responsavelTec(String value) {
+    _responsavelTec = value;
+  }
+
+  String get endere => _endere;
+
+  set endere(String value) {
+    _endere = value;
+  }
+
+  String get cnpj => _cnpj;
+
+  set cnpj(String value) {
+    _cnpj = value;
+  }
+
+  String get executor => _executor;
+
+  set executor(String value) {
+    _executor = value;
+  }
+
+  String get email => _email;
+
+  set email(String value) {
+    _email = value;
+  }
+
+  String get telefone => _telefone;
+
+  set telefone(String value) {
+    _telefone = value;
+  }
+
+  String get numero => _numero;
+
+  set numero(String value) {
+    _numero = value;
+  }
+
+  String get bairro => _bairro;
+
+  set bairro(String value) {
+    _bairro = value;
+  }
+
+  String get UF => _UF;
+
+  set UF(String value) {
+    _UF = value;
+  }
+
+  String get cidade => _cidade;
+
+  set cidade(String value) {
+    _cidade = value;
+  }
+
+  set endCompleto(String value) {
+    _endCompleto = value;
+  }
+
+
+}
